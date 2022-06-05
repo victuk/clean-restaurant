2f0ce20af270007833f6166851e567267fd86bd5
Victor <ukokjnr@gmail.com>
2022-06-05 21:39:07 +0100

    Change reservation to Go to menu

--- js/checkOut.js
+++ js/checkOut.js
@@ -1,4 +1,10 @@
-const name = document.getElementById('name');
+const personsName = document.getElementById('name');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const address = document.getElementById('address');
+const submitButton = document.getElementById('submit');
+
+submitButton.addEventListener('click', function() {
+    console.log(personsName, email, phone, address);
+    alert('Order Successful');
+});
